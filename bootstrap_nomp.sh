1bacd5a2be04504c7eb16ab7ea72eb2b0c47cd8d
tiranad <35414316+tiranad@users.noreply.github.com>
2021-03-13 13:12:39 -0700

    Update bootstrap_nomp.sh

--- bootstrap_nomp.sh
+++ bootstrap_nomp.sh
@@ -16,7 +16,7 @@ if [ ! -d $HOME/multipool/nomp ]; then
 	echo Downloading MultiPool NOMP Server Installer ${TAG}. . .
 	git clone \
 		-b ${TAG} --depth 1 \
-		https://github.com/cryptopool-builders/multipool_nomp \
+		https://github.com/tiranad/multipool_nomp \
 		$HOME/multipool/nomp \
 		< /dev/null 2> /dev/null
 
